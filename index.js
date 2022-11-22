5cd1faba8885ef26593255ce4810ce89a73fbb3f
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-22 21:44:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1= 31;
+let num2= 2;
+let num3 = 19;
+let num4 = 5;
+var multiply = num1 * num2;
 
-// Write your code here
+var random = Math.random(Number.isInteger(576));
+
+
+var mod = num3 % num4;
+
+var max = Math.max(1, 2, 13, 14, 20);
+
+
+
